f42b2c6fd64770f8f8eb92d49805c3d4210cb547
Dylan Cancelliere <dylan.cancelliere@bryx.com>
2025-09-19 01:53:32 -0400

    notes, fix dumb pipeline fail

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import { Timer } from "./Timer";
 import { Button, Stack } from "@mantine/core";
 
 function App() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const userId = urlParams.get("userId");
-
   return (
     <Stack h="100%" w="100%" align="center">
       <Stack h="100%" justify="center">
